331d8503f465d9ae10223cf3ce335b2d98c59bac
Nazym Berdaly <nazym.berdalyeva@gmail.com>
2025-02-08 21:58:09 -0800

    feat: create coffee repository

--- packages/coffee_repository/lib/src/coffee_repository.dart
+++ packages/coffee_repository/lib/src/coffee_repository.dart
@@ -1,15 +1,32 @@
+import 'dart:io';
+
+import 'package:db_client/db_client.dart';
+
 /// {@template coffee_repository}
 /// A Very Good Project created by Very Good CLI.
 /// {@endtemplate}
 class CoffeeRepository {
   /// {@macro coffee_repository}
-  const CoffeeRepository();
+  const CoffeeRepository({required DbClient dbClient,
+  }) : _dbClient = dbClient;
 
-  // Future<Map<String, String>> fetchFavoriteCoffeeImage() async {
-  //   try {
-      
-  //   } catch (_) {
+  final DbClient _dbClient;
 
-  //   }
-  // }
+  Future<void> addCoffee(String name, File imageFile) async {
+    final imagePath = await _dbClient.saveImage(imageFile, name);
+    final coffee = CoffeeEntity(imagePath: imagePath);
+    await _dbClient.insertCoffee(coffee);
+  }
+
+  Future<List<CoffeeEntity>> getFavoriteCoffees() {
+    return _dbClient.getCoffees();
+  }
+
+   Future<void> removeCoffee(int id) async {
+    await _dbClient.deleteCoffee(id);
+  }
+
+   Future<CoffeeEntity?> getCoffee(int id) async {
+    return _dbClient.getCoffee(id);
+  }
 }
